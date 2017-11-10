052e9e943cf28eeca2251771d1f722c6f8bd6043
akshay <aaravind@syr.edu>
2017-11-10 15:29:49 -0500

    Added chunkHandle (for testing purposes)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,19 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    private final String root = "/Users/akshay/Documents/MacSystem/MS/1sem/Operating-Systems/Lab/Project3/gfs";
+
+    public Result index() {
+        return ok(index.render("GFS....."));
+    }
+
+    public Result chunkHandle(String filename, String chunkIndex) {
+        return ok(new java.io.File("/build.sbt"));
     }
 
 }
