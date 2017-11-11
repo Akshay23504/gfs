1db0280298d86e527d1bbbdc897a914e3821fa28
Jakob Zeitler <jz349@exeter.ac.uk>
2017-11-10 22:16:04 -0500

    LOL

---
+++ app/ApplicationStart.java
@@ -0,0 +1,20 @@
+import javax.inject.*;
+import play.inject.ApplicationLifecycle;
+import play.Environment;
+import java.util.concurrent.CompletableFuture;
+
+// This creates an `ApplicationStart` object once at start-up.
+@Singleton
+public class ApplicationStart {
+
+    // Inject the application's Environment upon start-up and register hook(s) for shut-down.
+    @Inject
+    public ApplicationStart(ApplicationLifecycle lifecycle, Environment environment) {
+        System.out.println("LOL");
+        // Shut-down hook
+        lifecycle.addStopHook( () -> {
+            return CompletableFuture.completedFuture(null);
+        } );
+        // ...
+    }
+}
--- app/controllers/ChunkServer.java
+++ app/controllers/ChunkServer.java
@@ -5,6 +5,13 @@ import play.mvc.Result;
 
 public class ChunkServer extends Controller {
 
+    String projectRoot = Play.applicationPath;
+    String chunksRoot
+
+    public Result poll() {
+        return ok();
+    }
+
     public Result poll() {
         return ok();
     }
