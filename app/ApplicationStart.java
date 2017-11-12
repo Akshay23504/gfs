52640bbaa875df8b763462d1b06785e772b1993f
Jakob Zeitler <jz349@exeter.ac.uk>
2017-11-11 23:00:10 -0500

    Implemented chunkServer#poll

--- app/ApplicationStart.java
+++ app/ApplicationStart.java
@@ -17,17 +17,19 @@ public class ApplicationStart {
     // Inject the application's Environment upon start-up and register hook(s) for shut-down.
     @Inject
     public ApplicationStart(ApplicationLifecycle lifecycle, Environment environment, WSClient wsClient) {
-        System.out.println("Registering with master");
-        lifecycle.addStopHook( () -> CompletableFuture.completedFuture(null));
+        if (0==1) { //Instance is chunkserver
+            System.out.println("Registering with master");
+            lifecycle.addStopHook(() -> CompletableFuture.completedFuture(null));
 
-        WSRequest request = wsClient.url("http://localhost:9000/master/registerChunkServer");
-        CompletionStage<String> responsePromise = request.get().thenApply(WSResponse::getBody);
-        try {
-            System.out.print(responsePromise.toCompletableFuture().get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+            WSRequest request = wsClient.url("http://localhost:9000/master/registerChunkServer");
+            CompletionStage<String> responsePromise = request.get().thenApply(WSResponse::getBody);
+            try {
+                System.out.print(responsePromise.toCompletableFuture().get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- app/controllers/ChunkServer.java
+++ app/controllers/ChunkServer.java
@@ -1,14 +1,45 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.api.Play;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.io.File;
+
+import javax.inject.*;
+import play.libs.ws.*;
+import play.Environment;
+
 public class ChunkServer extends Controller {
+
+    @Inject Environment environment;
 
     public Result poll() {
         //TODO: read in all hunks
         //TODO: create list of the unique ids of all chunks
         //TODO: return the list of the chunk unique ids
-        return ok();
+
+        File folder = new File(environment.rootPath() + "/chunks");
+        File[] listOfFiles = folder.listFiles();
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        ObjectNode result = Json.newObject();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                arrayNode.add(listOfFiles[i].getName());
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        result.set("chunks", arrayNode);
+
+        return ok(result);
     }
 }
