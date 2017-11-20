4bea79b8a8927a116e07c8992e94708345944a00
Jakob Zeitler <jz349@exeter.ac.uk>
2017-11-20 17:32:10 +0000

    Big changes

--- app/controllers/ChunkServer.java
+++ app/controllers/ChunkServer.java
@@ -9,7 +9,13 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
-import java.io.File;
+import java.io.*;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class ChunkServer extends Controller {
 
@@ -20,13 +26,17 @@ public class ChunkServer extends Controller {
         this.environment = environment;
     }
 
+    public String getChunksPath() {
+        return environment.rootPath() + "/chunks" +  System.getProperty("http.port") + "/";
+    }
+
     public Result poll() {
         //TODO: read in all chunks
         //TODO: create list of the unique ids of all chunks
         //TODO: return the list of the chunk unique ids
         //TODO: Maybe create a model when we have time
 
-        File folder = new File(environment.rootPath() + "/chunks");
+        File folder = new File(getChunksPath());
         File[] listOfFiles = folder.listFiles();
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode arrayNode = mapper.createArrayNode();
@@ -42,5 +52,41 @@ public class ChunkServer extends Controller {
         }
         result.set("chunks", arrayNode);
         return ok(result);
+    }
+
+    public Result writeChunk(String uuid) {
+        DataOutputStream os = null;
+        try {
+            os = new DataOutputStream(new FileOutputStream(getChunksPath() + uuid));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            os.writeInt(123);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ok();
+
+    }
+
+    public Result readChunk(String uuid) {
+
+        Path path = Paths.get(getChunksPath() + uuid);
+        byte[] data = new byte[64];
+        try {
+            data = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.print(Arrays.toString(data));
+
+        return ok(data).as("application/octet-stream");
+
     }
 }
