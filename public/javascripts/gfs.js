5c28faa8792a28037142450c074d9c484d556b4c
akshay <aaravind@syr.edu>
2017-11-23 20:24:30 -0500

    Fixed auto reload duplicating chunkserver list and stop button

--- public/javascripts/gfs.js
+++ public/javascripts/gfs.js
@@ -2,22 +2,24 @@ function getChunkServers() {
     $("#chunkServerTableBody").html("");
     $.get("/master/getChunkServers", function(data) {
         $.each(data.chunkServers[0], function(index, value) {
+            if (index == 0) $("#chunkServerTableBody").html("");
             var ip = "/master/chunkServerDead?ip=" + value.ip + "&port=" + value.port;
             $("#chunkServerTableBody").append("" +
                 "<tr>" +
                     "<td scope='row'>" + value.ip + ":" + value.port + "</td>" +
                     "<td scope='row'>" + "chunks" + "</td>" +
                     "<td scope='row'>" + value.status + "</td>" +
-                    "<td scope='row'><a href='' + class='btn btn-danger startOrStopButton'>Stop</a></td>" +
+                    "<td scope='row'><a href='' id=" + value.port + " class='btn btn-danger startOrStopButton'>Stop</a></td>" +
                 "</tr>"
             );
-            $('.startOrStopButton').click(function(event) {
+            $('#' + value.port).click(function(event) {
                 event.preventDefault();
                 $.get(ip, function () {
                     getChunkServers();
                 });
             });
         });
-        setTimeout(getChunkServers, 10000);
     });
 }
+
+setInterval(getChunkServers, 10000);
